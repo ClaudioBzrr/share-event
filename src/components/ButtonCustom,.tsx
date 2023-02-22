3a57e872b676b9e3195883709949772527353d57
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-02-22 11:14:49 -0300

    update form page core

--- src/components/ButtonCustom,.tsx
+++ src/components/ButtonCustom,.tsx
@@ -6,7 +6,10 @@ interface ButtonCustomProps extends ButtonHTMLAttributes<HTMLButtonElement>{
 
 export function ButtonCustom({title,...rest}:ButtonCustomProps){
     return(
-        <button {...rest}>
+        <button
+            className="bg-slate-500 p-2 rounded-lg font-semibold hover:bg-slate-600"
+            {...rest}
+        >
             {title}
         </button>
     )
--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -2,14 +2,16 @@ import { InputCustom } from "../components/InputCustom";
 import { useRef, useState } from "react";
 import { format } from "date-fns";
 import { ButtonCustom } from "../components/ButtonCustom,";
-
+import {Image} from 'phosphor-react'
+import {useNavigate} from 'react-router-dom'
 
 export function Form(){
+    const navigate =  useNavigate()
     const inputHidden =  useRef <HTMLInputElement|null>(null)
     const [eventTitle,setEventTitle] =  useState<string>('')
     const [eventDescription,setEventDescription] = useState<string>('')
     const [eventDate,setEventDate] =  useState<string>(format(new Date,'dd/MM/yyyy'))
-    const [imageUrl,setImageUrl] =  useState<string>('')
+    const [imageBase64,setImageBase64] =  useState<string>('')
 
     function handleClickToSelectImage(e:Event){
         e.preventDefault()
@@ -18,47 +20,69 @@ export function Form(){
 
     function handleSelectImages(image:FileList){
         if(image.length == 1){
-            const imageBlob =  new Blob([image[0]],{type:'image/bmp'})
-            const imageBlobURL =  URL.createObjectURL(imageBlob)
-            setImageUrl(imageBlobURL)
+            const reader =  new FileReader()
+            reader.addEventListener('load',() => typeof reader.result == 'string' && setImageBase64(reader.result))
+            reader.readAsDataURL(image[0])
         }
+    }
+
+    function handleGenerateInvitation(){
+        localStorage.setItem('title',eventTitle)
+        localStorage.setItem('description',eventDescription)
+        localStorage.setItem('image',imageBase64)
+        navigate('/share')
     }
     return(
         <div className="bg-slate-800 h-[100vh] w-[100vw] flex flex-row items-center justify-center">
-            <div className="h-[70vh] w-[30vw] border-solid border-slate-900 border-[1px] rounded-xl">
-                <div>
-                    <label className="font-semibold text-white">Título do evento : </label>
-                    <InputCustom 
-                        onChange={e => setEventTitle(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label className="font-semibold text-white" >Descrição : </label>
-                    <InputCustom
-                        onChange={e => setEventDescription(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label className="font-semibold text-white" >Data do evento : </label>
-                    <InputCustom 
-                        placeholder={eventDate}
-                        onChange={e => setEventDate(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <ButtonCustom
-                        title="Escolher foto para o evento"
-                        onClick={e => handleClickToSelectImage(e.nativeEvent)}
-                    />
-                    <input 
-                        ref={inputHidden}
-                        type='file'
-                        accept="image/*"
-                        required
-                        className="hidden"
-                        onChange={e => handleSelectImages(e.target.files!)}
-                    />
-                </div>
+            <div className="h-[70vh] w-[30vw] border-solid border-slate-900 border-[1px] rounded-xl flex flex-col items-center justify-center">
+                <section className="w-auto flex flex-col items-end justify-center">
+                    <div className="mt-4">
+                        <label className="font-semibold text-white">Título do evento : </label>
+                        <InputCustom 
+                            onChange={e => setEventTitle(e.target.value)}
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <label className="font-semibold text-white" >Descrição : </label>
+                        <InputCustom
+                            onChange={e => setEventDescription(e.target.value)}
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <label className="font-semibold text-white" >Data do evento : </label>
+                        <InputCustom 
+                            placeholder={eventDate}
+                            onChange={e => setEventDate(e.target.value)}
+                        />
+                    </div>
+                    <div className="mt-4 self-center w-auto flex flex-row items-center justify-center">
+                        {
+                            imageBase64 !="" ? (
+                                <img src={imageBase64} className="w-9 h-9 mr-3 rounded-lg" alt="Imagem selecionada" />
+                            ):(
+                                <Image size={48} className="text-slate-500"/>
+                            )
+                        }
+                        <ButtonCustom
+                            title={imageBase64 =="" ? "Escolher foto para o evento" : "Alterar foto"}
+                            onClick={e => handleClickToSelectImage(e.nativeEvent)}
+                        />
+                        <input 
+                            ref={inputHidden}
+                            type='file'
+                            accept="image/*"
+                            required
+                            className="hidden"
+                            onChange={e => handleSelectImages(e.target.files!)}
+                        />
+                    </div>
+                    <div className="self-center mt-24">
+                        <ButtonCustom 
+                            title="Gerar Convite"
+                            onClick={handleGenerateInvitation}
+                        />
+                    </div>
+                </section>
             </div>
         </div>
     )
